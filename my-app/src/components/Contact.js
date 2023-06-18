1661cb5dad914ebca520e33c586bf8e9cdeb8c38
Kels <104536953+xkellss@users.noreply.github.com>
2023-06-18 13:58:51 -0500

    .

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -1,61 +1,125 @@
-import React, { useState } from 'react';
-import classes from './Contact.module.css'
-import { IoIosContact } from 'react-icons/io';
 
+import React from "react";
 
+export default function Contact() {
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [message, setMessage] = React.useState("");
 
-function Contact(){
+    function encode(data) {
+        return Object.keys(data)
+            .map(
+                (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+            )
+            .join("&");
+    }
 
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
-
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault();
-        const myForm = e.target;
-        const formData = new FormData(myForm);
-
         fetch("/", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: new URLSearchParams(formData).toString(),
+            body: encode({ "form-name": "contact", name, email, message }),
         })
-            .then(() => alert("Thank you for your submission"))
+            .then(() => alert("Message sent!"))
             .catch((error) => alert(error));
     }
 
-
-    return(
-        <section id="contact">
-            <div className={classes.contactContainer}>
-                <span className={classes.contactLogo}><IoIosContact/></span>
-
-                <form name="contact" method="POST" netlify>
-                    <input type="hidden" name="form-name" value="contact"/>
-                    <p>
-                        <label>Your Name: <input type="text" name="name" /></label>
+    return (
+        <section id="contact" className="relative">
+            <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+                <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+                    <iframe
+                        width="100%"
+                        height="100%"
+                        title="map"
+                        className="absolute inset-0"
+                        frameBorder={0}
+                        marginHeight={0}
+                        marginWidth={0}
+                        style={{ filter: "opacity(0.7)" }}
+                        src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+                    />
+                    <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+                        <div className="lg:w-1/2 px-6">
+                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                                ADDRESS
+                            </h2>
+                            <p className="mt-1">
+                                97 Warren St. <br />
+                                New York, NY 10007
+                            </p>
+                        </div>
+                        <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                            <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                                EMAIL
+                            </h2>
+                            <a className="text-indigo-400 leading-relaxed">
+                                reedbarger@email.com
+                            </a>
+                            <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                                PHONE
+                            </h2>
+                            <p className="leading-relaxed">123-456-7890</p>
+                        </div>
+                    </div>
+                </div>
+                <form
+                    netlify
+                    name="contact"
+                    onSubmit={handleSubmit}
+                    className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+                        Hire Me
+                    </h2>
+                    <p className="leading-relaxed mb-5">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+                        suscipit officia aspernatur veritatis. Asperiores, aliquid?
                     </p>
-                    <p>
-                        <label>Your Email: <input type="email" name="email" /></label>
-                    </p>
-                    <p>
-                        <label>Your Role: <select name="role[]" multiple>
-                            <option value="leader">Leader</option>
-                            <option value="follower">Follower</option>
-                        </select></label>
-                    </p>
-                    <p>
-                        <label>Message: <textarea name="message"></textarea></label>
-                    </p>
-                    <p>
-                        <button type="submit">Send</button>
-                    </p>
+                    <div className="relative mb-4">
+                        <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+                            Name
+                        </label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    <div className="relative mb-4">
+                        <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="relative mb-4">
+                        <label
+                            htmlFor="message"
+                            className="leading-7 text-sm text-gray-400">
+                            Message
+                        </label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                            onChange={(e) => setMessage(e.target.value)}
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                        Submit
+                    </button>
                 </form>
-
             </div>
-
         </section>
-    )
-
-
-}export default Contact;
+    );
+}
