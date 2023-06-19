57a0d06a3753dc1ab5cb808f1e9254fc318f3255
Kels <104536953+xkellss@users.noreply.github.com>
2023-06-19 14:48:50 -0500

    .

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -1,54 +1,58 @@
 import React, { useState } from 'react';
+import classes from './Contact.module.css'
+import { IoIosContact } from 'react-icons/io';
 
-function Contact() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
 
-    const handleSubmit = (e) => {
+
+function Contact(){
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    function handleSubmit(e){
         e.preventDefault();
-        const form = e.target;
-        const formData = new FormData(form);
+        const myForm = e.target;
+        const formData = new FormData(myForm);
 
-        fetch('/', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
             body: new URLSearchParams(formData).toString(),
         })
-            .then(() => alert('Thank you for your submission'))
+            .then(() => alert("Thank you for your submission"))
             .catch((error) => alert(error));
-    };
+    }
 
-    return (
+
+    return(
         <section id="contact">
-            <div>
-                <h2>Contact me</h2>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        name="name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        placeholder="Your name"
-                    />
-                    <input
-                        type="email"
-                        name="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder="Your email"
-                    />
-                    <textarea
-                        name="message"
-                        value={message}
-                        onChange={(e) => setMessage(e.target.value)}
-                        placeholder="Your message"
-                    >'</textarea>
-                    <button type="submit">Submit</button>
+            <div className={classes.contactContainer}>
+                <span className={classes.contactLogo}><IoIosContact/></span>
+                <form  className={classes.form}  onSubmit={handleSubmit} >
+                    <h2 className={classes.contactText} >Contact me!</h2>
+                    <p className={classes.summary}> You can contact me by messaging me on <a href="https://www.linkedin.com/in/raquel-gonzalez-6b4a05219/" target="_blank" >Linkedin </a>
+                        or filling out the form below :)</p>
+                    <div className={classes.contactInput}>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" id="name" name="name" placeholder={"Raquel Gonzalez(Kels)"} value={name} onChange={(e => setName(e.target.value))}/>
+                    </div>
+                    <div className={classes.contactInput}>
+                    <label htmlFor="email">E-mail</label>
+                        <input type="email" id="email" name="email" placeholder={"Raquel@gmail.com"} value={email} onChange={(e => setEmail(e.target.value))}/>
+                    </div>
+                    <div className={classes.contactInput}>
+                    <label htmlFor="message">Message</label>
+                        <textarea  name="message" id="message" placeholder={"I found your portfolio interesting and I would like to " +
+                        "schedule a call whenever you're available!"} value={message} onChange={(e => setMessage(e.target.value))}/>
+                    </div>
+                    <button type={"submit"}> Submit</button>
                 </form>
-            </div>
-        </section>
-    );
-}
 
-export default Contact;
+            </div>
+
+        </section>
+    )
+
+
+}export default Contact;
