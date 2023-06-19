b837ca9b4bf71dd3c3490b0bed8c67f8ab582dab
Kels <104536953+xkellss@users.noreply.github.com>
2023-06-19 14:36:55 -0500

    .

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -1,19 +1,18 @@
 import React, { useState } from 'react';
-import classes from './Contact.module.css'
+import classes from './Contact.module.css';
 import { IoIosContact } from 'react-icons/io';
 
-
-
-function Contact(){
-
+function Contact() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
 
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault();
-        const myForm = e.target;
-        const formData = new FormData(myForm);
+        const formData = new FormData();
+        formData.append("name", name);
+        formData.append("email", email);
+        formData.append("message", message);
 
         fetch("/", {
             method: "POST",
@@ -24,35 +23,30 @@ function Contact(){
             .catch((error) => alert(error));
     }
 
-
-    return(
+    return (
         <section id="contact">
             <div className={classes.contactContainer}>
-                <span className={classes.contactLogo}><IoIosContact/></span>
-                <form name={"contact"} className={classes.form}  onSubmit={handleSubmit} >
-                    <h2 className={classes.contactText} >Contact me!</h2>
-                    <p className={classes.summary}> You can contact me by messaging me on <a href="https://www.linkedin.com/in/raquel-gonzalez-6b4a05219/" target="_blank" >Linkedin </a>
-                        or filling out the form below :)</p>
+                <span className={classes.contactLogo}><IoIosContact /></span>
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <h2 className={classes.contactText}>Contact me!</h2>
+                    <p className={classes.summary}>You can contact me by messaging me on <a href="https://www.linkedin.com/in/raquel-gonzalez-6b4a05219/" target="_blank">Linkedin</a> or filling out the form below :)</p>
                     <div className={classes.contactInput}>
                         <label htmlFor="name">Name</label>
-                        <input type="text" id="name" name="name" placeholder={"Raquel Gonzalez(Kels)"} value={name} onChange={(e => setName(e.target.value))}/>
+                        <input type="text" id="name" name="name" placeholder={"Raquel Gonzalez(Kels)"} value={name} onChange={(e) => setName(e.target.value)} />
                     </div>
                     <div className={classes.contactInput}>
-                    <label htmlFor="email">E-mail</label>
-                        <input type="email" id="email" name="email" placeholder={"Raquel@gmail.com"} value={email} onChange={(e => setEmail(e.target.value))}/>
+                        <label htmlFor="email">E-mail</label>
+                        <input type="email" id="email" name="email" placeholder={"Raquel@gmail.com"} value={email} onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div className={classes.contactInput}>
-                    <label htmlFor="message">Message</label>
-                        <textarea  name="message" id="message" placeholder={"I found your portfolio interesting and I would like to " +
-                        "schedule a call whenever you're available!"} value={message} onChange={(e => setMessage(e.target.value))}/>
+                        <label htmlFor="message">Message</label>
+                        <textarea name="message" id="message" placeholder={"I found your portfolio interesting and I would like to schedule a call whenever you're available!"} value={message} onChange={(e) => setMessage(e.target.value)} />
                     </div>
-                    <button type={"submit"}> Submit</button>
+                    <button type="submit">Submit</button>
                 </form>
-
             </div>
-
         </section>
-    )
+    );
+}
 
-
-}export default Contact;
+export default Contact;
