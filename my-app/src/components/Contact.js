6f8a3522be582e4c80408026faf95a553d7908cd
Kels <104536953+xkellss@users.noreply.github.com>
2023-06-19 14:40:16 -0500

    .

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -9,10 +9,7 @@ function Contact() {
 
     function handleSubmit(e) {
         e.preventDefault();
-        const formData = new FormData();
-        formData.append("name", name);
-        formData.append("email", email);
-        formData.append("message", message);
+        const formData = new FormData(e.target);
 
         fetch("/", {
             method: "POST",
@@ -26,21 +23,24 @@ function Contact() {
     return (
         <section id="contact">
             <div className={classes.contactContainer}>
-                <span className={classes.contactLogo}><IoIosContact /></span>
-                <form className={classes.form} onSubmit={handleSubmit}>
+                <span className={classes.contactLogo}><IoIosContact/></span>
+                <form name="contact" className={classes.form} onSubmit={handleSubmit}>
                     <h2 className={classes.contactText}>Contact me!</h2>
-                    <p className={classes.summary}>You can contact me by messaging me on <a href="https://www.linkedin.com/in/raquel-gonzalez-6b4a05219/" target="_blank">Linkedin</a> or filling out the form below :)</p>
+                    <p className={classes.summary}>
+                        You can contact me by messaging me on{" "}
+                        <a href="https://www.linkedin.com/in/raquel-gonzalez-6b4a05219/" target="_blank">Linkedin</a> or filling out the form below :)
+                    </p>
                     <div className={classes.contactInput}>
                         <label htmlFor="name">Name</label>
-                        <input type="text" id="name" name="name" placeholder={"Raquel Gonzalez(Kels)"} value={name} onChange={(e) => setName(e.target.value)} />
+                        <input type="text" id="name" name="name" placeholder="Raquel Gonzalez(Kels)" value={name} onChange={(e) => setName(e.target.value)} />
                     </div>
                     <div className={classes.contactInput}>
                         <label htmlFor="email">E-mail</label>
-                        <input type="email" id="email" name="email" placeholder={"Raquel@gmail.com"} value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <input type="email" id="email" name="email" placeholder="Raquel@gmail.com" value={email} onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div className={classes.contactInput}>
                         <label htmlFor="message">Message</label>
-                        <textarea name="message" id="message" placeholder={"I found your portfolio interesting and I would like to schedule a call whenever you're available!"} value={message} onChange={(e) => setMessage(e.target.value)} />
+                        <textarea name="message" id="message" placeholder="I found your portfolio interesting and I would like to schedule a call whenever you're available!" value={message} onChange={(e) => setMessage(e.target.value)} />
                     </div>
                     <button type="submit">Submit</button>
                 </form>
