bccd390d93f7255b510505ac06d923219fcc300a
Kels <104536953+xkellss@users.noreply.github.com>
2023-06-19 14:50:29 -0500

    .

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -46,7 +46,7 @@ function Contact(){
                         <textarea  name="message" id="message" placeholder={"I found your portfolio interesting and I would like to " +
                         "schedule a call whenever you're available!"} value={message} onChange={(e => setMessage(e.target.value))}/>
                     </div>
-                    <button type={"submit"}> Submit</button>
+                    {/*<button type={"submit"}> Submit</button>*/}
                 </form>
 
             </div>
