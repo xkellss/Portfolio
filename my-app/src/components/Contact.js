3cacc523fa2052a240ac0f222d8a2df3b1f006c9
Kels <104536953+xkellss@users.noreply.github.com>
2023-06-19 15:07:52 -0500

    .

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -15,7 +15,7 @@ function Contact(){
         const myForm = e.target;
         const formData = new FormData(myForm);
 
-        fetch("/thank-you.html", {
+        fetch("/thank-you", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
             body: new URLSearchParams(formData).toString(),
